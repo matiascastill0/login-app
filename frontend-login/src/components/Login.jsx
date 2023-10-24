ee11c00a96e80a67e05522cf5fda653a456fa5d0
mateonoel2 <mateo.noel@utec.edu.pe>
2023-10-23 21:54:40 -0500

    Initial commit

---
+++ frontend-login/src/components/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const backendUrl = import.meta.env.VITE_APP_BACKEND_URL
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${backendUrl}/users/signin`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
+      });
+
+      const data = await response.text();
+
+      if (response.ok) {
+        setMessage('Login successful! Token: ' + data);
+      } else {
+        setMessage('Login failed! ' + data);
+      }
+    } catch (error) {
+      setMessage('An error occurred: ' + error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
